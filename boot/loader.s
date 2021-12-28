814a770cda8dca156d01bd283640cdace6197c12
Xgg <1366277024@qq.com>
2021-12-28 21:32:10 +0800

    加载并初始化内核

--- boot/loader.s
+++ boot/loader.s
@@ -140,9 +140,14 @@ loader_start:
     mov ax,SELECTOR_VIDEO
     mov gs,ax
     
-    mov byte [gs:160],'P'
-
-
+    mov byte [gs:160],'A'
+    mov byte [gs:160],'B'
+;---------------------------------加载内核elf文件到内存----------------------------
+    mov eax, KERNEL_BIN_SECTOR
+    mov ebx, KERNEL_BIN_BASE_ADDR
+    
+    mov ecx,200
+    call rd_disk_m_32
 
 ;----------------创建页目录和2个页表---------------------
 call setup_pageTable
@@ -168,64 +173,217 @@ mov cr0,eax
 
 lgdt [gdt_ptr]
 
-mov byte [gs:160],'V'
+mov byte [gs:160],'C'
 
-jmp $    
+jmp SELECTOR_CODE:enter_kernel
+    
+;------------------------------ 跳转到内核区    
+
+enter_kernel:
+    call kernel_init					          ;根据我们的1M以下的内存分布区 综合考虑出的数据
+    mov  esp,0xc009f000
+    mov byte [gs:160],'S'
+    jmp  KERNEL_ENTER_ADDR
  
  ;----------------------------创建页目录-------------------------------
 setup_pageTable:
- ;清空页目录所占内存的数据，逐个字节清0
-   mov ecx, 4096
-   mov esi, 0
-.clear_page_dir:
-   mov byte [PAGE_DIR_TABLE_POS + esi] , 0
-   inc esi 
-   loop .clear_page_dir
+mov ecx,0x1000                                             ;循环4096次 将页目录项清空 内存清0
+    mov esi,0                                                   
+ .clear_page_dir_mem:                                          ;dir directory 把页目录项清空
+    mov byte [PAGE_DIR_TABLE_POS+esi],0
+    inc esi
+    loop .clear_page_dir_mem
+    
+ .create_pde: 
+    mov eax,PAGE_DIR_TABLE_POS				  ;页目录项 起始位置
+    add eax,0x1000                                              ;页目录项刚好4k字节 add eax即得第一个页表项的地址
+                                                                ;接下来我们要做的是 把虚拟地址1M下和3G+1M 两部分的1M内存在页目录项中都映射到物理地址0-0XFFFFF
+    or  eax, PG_P | PG_RW_W | PG_US_U                           ;哦 悟了 哈哈哈 这里设置为PG_US_U 是因为init在用户进程 如果这里设置成US_S 这样子连进内核都进不去了
+     
+    mov [PAGE_DIR_TABLE_POS+0x0],eax                             ;页目录项偏移0字节与偏移0xc00 对应0x 一条页目录项对应2^22位4MB 偏移由前10位*4字节得到 可自己推算一下
+    mov [PAGE_DIR_TABLE_POS+0xc00],eax                        
+    sub eax,0x1000      
+    
+    mov [PAGE_DIR_TABLE_POS+4092],eax                           ;虚拟内存最后一个目录项 指向页目录表自身 书上写的是为了动态操纵页表 我也不是很清楚 反正有用 先放放
 
-;创建页目录
-.create_pde:
-   mov eax,PAGE_DIR_TABLE_POS
-   add eax,0x1000  ;0X1000+EAX是第一个页表的地址
-   mov ebx,eax
-   or  eax, PG_US_U | PG_RW_W | PG_P
+;这里就创建了一页页表    
+    mov eax,PAGE_DIR_TABLE_POS
+    add eax,0x1000
+    mov ecx,256
+    mov esi,0
+    mov ebx,PG_P | PG_RW_W | PG_US_U 
+    
+ .create_kernel_pte:           
+    mov [eax+esi*4],ebx
+    inc esi
+    add ebx,0x1000
+    loop .create_kernel_pte 
+    
+    
+;这里对于我们这里填写的目录项所对应的页表 页表中我们还没填写的值
+;为了实现 真正意义上的 内核空间被用户进程完全共享
+;只是把页目录与页表的映射做出来了 
+
+    mov eax,PAGE_DIR_TABLE_POS
+    add eax,0x2000       					   ;eax此时处于第二个页表
+    or  eax,PG_P | PG_RW_W | PG_US_U
+;这里循环254次可以来分析一下 我们这里做的是 0xc0 以上部分的映射    0xc0 对应的是第768个页表项 页表项中一共有 2^10=1024项
+;第1023项我们已经设置成 映射到页目录项本身位置了 即1022 - 769 +1 = 254
+    mov ebx,PAGE_DIR_TABLE_POS
+    mov ecx,254						  
+    mov esi,769
+        
+ .create_kernel_pde:
+    mov [ebx+esi*4],eax
+    inc esi
+    add eax,0x1000
+    loop .create_kernel_pde 
+    
+    ret    
+;-------------------初始化内核-----------------------------------------------
+;----------------------- 初始化内核 把缓冲区的内核代码放到0x1500区域 ------------------------------------------
+;这个地方主要对elf文件头部分用的很多
+;可以参照着书上给的格式 来比较对比
+kernel_init:
+    xor eax,eax   ;全部清零
+    xor ebx,ebx
+    xor ecx,ecx
+    xor edx,edx
+    
+    ;这里稍微解释一下 因为0x70000 为64kb*7=448kb 而我们的内核映射区域是4MB 而在虚拟地址4MB以内的都可以当作1:1映射
+    mov ebx,[KERNEL_BIN_BASE_ADDR+28]
+    add ebx,KERNEL_BIN_BASE_ADDR                               ;ebx当前位置为程序段表
+    mov dx,[KERNEL_BIN_BASE_ADDR+42]		         ;获取程序段表每个条目描述符字节大小
+    mov cx,[KERNEL_BIN_BASE_ADDR+44]                         ;一共有几个段
+    
+     
+ .get_each_segment:
+    cmp dword [ebx+0],PT_NULL
+    je .PTNULL                                                 ;空即跳转即可 不进行mem_cpy
+    
+    mov eax,[ebx+8]
+    cmp eax,0xc0001500
+    jb .PTNULL
+    
+        
+    push dword [ebx+16]                                        ;ebx+16在存储的数是filesz  可以翻到Loader刚开始
+                                                               
+    mov eax,[ebx+4]                                            
+    add eax,KERNEL_BIN_BASE_ADDR
+    push eax                                                   ;p_offset 在文件中的偏移位置    源位置         
+    push dword [ebx+8]                                         ;目标位置
+     
+    call mem_cpy
+    add esp,12                                                 ;把三个参数把栈扔出去 等于恢复栈指针
+    
+ .PTNULL:
+    add  ebx,edx                                               ;edx是一个描述符字节大小
+    loop .get_each_segment                                     ;继续进行外层循环    
+    ret
+                                        
+mem_cpy:
+    cld                                                        ;向高地址自动加数字 cld std 向低地址自动移动
+    push ebp                                                   ;保存ebp 因为访问的时候通过ebp 良好的编程习惯保存相关寄存器
+    mov  ebp,esp 
+    push ecx                                                   ;外层循环还要用 必须保存 外层eax存储着还有几个段
+    
+                                                               ;分析一下为什么是 8 因为进入的时候又重新push了ebp 所以相对应的都需要+4
+                                                               ;并且进入函数时 还Push了函数返回地址 所以就那么多了
+    mov edi,[ebp+8]                                            ;目的指针 edi存储的是目的位置 4+4
+    mov esi,[ebp+12]                                           ;源指针   源位置             8+4
+    mov ecx,[ebp+16]                                           ;与Movsb好兄弟 互相搭配      12+4
+    
+    
+    rep movsb                                                  ;一个一个字节复制
+       
+    pop ecx 
+    pop ebp
+    ret
    
-   mov [PAGE_DIR_TABLE_POS+0x0],eax
-  
-;设置内核空间地址
+;------------------------ rd_disk_m_32  在mbr.S复制粘贴过来的 修改了点代码 ----------------------
+rd_disk_m_32:
+;1 写入待操作磁盘数
+;2 写入LBA 低24位寄存器 确认扇区
+;3 device 寄存器 第4位主次盘 第6位LBA模式 改为1
+;4 command 写指令
+;5 读取status状态寄存器 判断是否完成工作
+;6 完成工作 取出数据
+ 
+ ;;;;;;;;;;;;;;;;;;;;;
+ ;1 写入待操作磁盘数
+ ;;;;;;;;;;;;;;;;;;;;;
+    mov esi,eax   ; !!! 备份eax
+    mov di,cx     ; !!! 备份cx
+    
+    mov dx,0x1F2  ; 0x1F2为Sector Count 端口号 送到dx寄存器中
+    mov al,cl     ; !!! 忘了只能由ax al传递数据
+    out dx,al     ; !!! 这里修改了 原out dx,cl
+    
+    mov eax,esi   ; !!!袄无! 原来备份是这个用 前面需要ax来传递数据 麻了
+    
+;;;;;;;;;;;;;;;;;;;;;
+;2 写入LBA 24位寄存器 确认扇区
+;;;;;;;;;;;;;;;;;;;;;
+    mov cl,0x8    ; shr 右移8位 把24位给送到 LBA low mid high 寄存器中
 
-   mov [PAGE_DIR_TABLE_POS + 0xc00],eax
+    mov dx,0x1F3  ; LBA low
+    out dx,al 
+    
+    mov dx,0x1F4  ; LBA mid
+    shr eax,cl    ; eax为32位 ax为16位 eax的低位字节 右移8位即8~15
+    out dx,al
+    
+    mov dx,0x1F5
+    shr eax,cl
+    out dx,al
+    
+;;;;;;;;;;;;;;;;;;;;;
+;3 device 寄存器 第4位主次盘 第6位LBA模式 改为1
+;;;;;;;;;;;;;;;;;;;;;
 
-;设置最后一个页表指向页目录,目的是为了将来可以动态操作页表
+    		 
+    		  ; 24 25 26 27位 尽管我们知道ax只有2 但还是需要按规矩办事 
+    		  ; 把除了最后四位的其他位置设置成0
+    shr eax,cl
+    
+    and al,0x0f 
+    or al,0xe0   ;!!! 把第四-七位设置成0111 转换为LBA模式
+    mov dx,0x1F6 ; 参照硬盘控制器端口表 Device 
+    out dx,al
 
-   sub eax,0x1000
-   mov [PAGE_DIR_TABLE_POS + 4092],eax
+;;;;;;;;;;;;;;;;;;;;;
+;4 向Command写操作 Status和Command一个寄存器
+;;;;;;;;;;;;;;;;;;;;;
 
-;开始创建第一个页表中的 ‘页表项’，第一个页表项负责映射0-4M
-   mov ecx ,256 ;1m/4k=256
-   mov esi,0
-   mov edx ,PG_US_U | PG_RW_W | PG_P
+    mov dx,0x1F7 ; Status寄存器端口号
+    mov ax,0x20  ; 0x20是读命令
+    out dx,al
+    
+;;;;;;;;;;;;;;;;;;;;;
+;5 向Status查看是否准备好惹 
+;;;;;;;;;;;;;;;;;;;;;
+    
+		   ;设置不断读取重复 如果不为1则一直循环
+  .not_ready:     
+    nop           ; !!! 空跳转指令 在循环中达到延时目的
+    in al,dx      ; 把寄存器中的信息返还出来
+    and al,0x88   ; !!! 0100 0100 0x88
+    cmp al,0x08
+    jne .not_ready ; !!! jump not equal == 0
+    
+    
+;;;;;;;;;;;;;;;;;;;;;
+;6 读取数据
+;;;;;;;;;;;;;;;;;;;;;
 
-.create_pte:
-   mov [ebx + esi*4],edx;因为第一个映射到0-4k,所以不用为edx设置映射的基址
-   add edx,4096  ;求得下一页的基址
-   inc esi
-   loop .create_pte
-
-
-;创建第768个页表的页表项
-   mov eax , PAGE_DIR_TABLE_POS
-   add eax,0x2000 ;eax指向第二个页表，也就是第页表项中的第768个页表
-   or eax,PG_US_U | PG_RW_W | PG_P
-   mov ebx,PAGE_DIR_TABLE_POS
-   mov ecx , 254
-   mov esi,769
-.create_kernel_pde:
-   mov [ebx+esi*4],eax
-   inc esi
-   add eax,0x1000
-   loop .create_kernel_pde
-   ret
-
-
-   
-
+    mov ax,di      ;把 di 储存的cx 取出来
+    mov dx,256
+    mul dx        ;与di 与 ax 做乘法 计算一共需要读多少次 方便作循环 低16位放ax 高16位放dx
+    mov cx,ax      ;loop 与 cx相匹配 cx-- 当cx == 0即跳出循环
+    mov dx,0x1F0
+ .go_read_loop:
+    in ax,dx      ;两字节dx 一次读两字
+    mov [ebx],ax
+    add ebx,2
+    loop .go_read_loop
+    ret ;与call 配对返回原来的位置 跳转到call下一条指令
---
+++ lib/stdint.h
@@ -0,0 +1,12 @@
+/*存放一些定义的数据结构*/
+#ifndef __LIB_STDINT_H
+#define __LIB_STDINT_H
+typedef signed char int8_t;
+typedef signed short int int16_t;
+typedef signed int int32_t;
+typedef signed long long int int64_t;
+typedef signed unsigned char uint8_t;
+typedef signed unsigned short int uint16_t;
+typedef signed unsigned int uint32_t;
+typedef signed unsigned long long int uint64_t;
+#endif
